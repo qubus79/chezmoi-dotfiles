388ecd2183bb3ed3ecdc7d0066042b2799d74641
Pawel Nocznicki <sales@nocznicki.pl>
2024-11-03 01:11:17 +0100

    changes to scripts

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Getting ohmyz.sh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
 else
 	echo ".oh-my-zsh already found, skipping."
+
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+	echo "Getting powerlevel10k"
+	git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+	echo "powerlevel10k already found, skipping."
+
 fi
